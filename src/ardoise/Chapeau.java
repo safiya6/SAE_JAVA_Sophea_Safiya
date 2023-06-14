9a2b2a245de0c0f81529a483ac2fa9f719780927
safiya6 <120652909+safiya6@users.noreply.github.com>
2023-06-14 11:19:14 +0200

    Add files via upload

---
+++ src/ardoise/Chapeau.java
@@ -0,0 +1,108 @@
+package ardoise;
+
+import java.util.ArrayList;
+
+/**
+ * Classe Chapeau
+ * <br>Modelise un chapeau compose de 3 PointPlan
+ * <br>Un chapeau est composee de 3 PointPlan relie avec 2 segments
+ */
+public class Chapeau extends Forme {
+	
+	private PointPlan POINT1;
+	private PointPlan POINT2;
+	private PointPlan POINT3;
+
+	/**
+	 *Constructeur par defaut, met tous les points a 0 et le nom par defaut est chapeau
+	 */
+	public Chapeau() {
+		super("chapeau");
+		this.POINT1 = new PointPlan(0,0);
+		this.POINT2 = new PointPlan(0,0);
+		this.POINT3 = new PointPlan(0,0);
+	}
+	
+	/**
+	 *Constructeur champ a champ
+	 */
+	public Chapeau(String nom, PointPlan p1, PointPlan p2, PointPlan p3) {
+		super(nom);
+		this.POINT1=p1;
+		this.POINT2=p2;
+		this.POINT3=p3;
+	}
+	
+	/**
+	 *Constructeur par copie
+	 */
+	public Chapeau(Chapeau C) {
+		this.setPoint1(C.getPoint1());
+		this.setPoint2(C.getPoint2());
+		this.setPoint3(C.getPoint3());
+		
+	}
+	
+	public PointPlan getPoint1() {
+		return this.POINT1;
+	}
+	
+	public PointPlan getPoint2() {
+		return this.POINT2;
+	}
+	
+	public PointPlan getPoint3() {
+		return this.POINT3;
+	}
+	
+	public void setPoint1(PointPlan p) {
+		this.POINT1=p;
+	}
+	
+	public void setPoint2(PointPlan p) {
+		this.POINT2=p;
+	}
+	
+	public void setPoint3(PointPlan p) {
+		this.POINT3=p;
+	}
+
+	/**
+	 *Un chapeau doit-etre capable de se deplacer
+	 *<br>Deplace le chapeau de deplacementX et de deplacementY
+	 *
+	 *@param deplacementX  - (int) : nombre de point(s) de deplacement en abscisse.
+	 *@param deplacementY  - (int) : nombre de point(s) de deplacement en ordonnée.
+	 */
+	@Override
+	public void deplacer(int x, int y) {
+		this.POINT1.deplacer(x,y);
+		this.POINT2.deplacer(x,y);
+		this.POINT3.deplacer(x,y);
+		
+	}
+
+	/**
+	 * Dessine le chapeau en renvoyant la liste des segments qui le compose.
+	 *
+	 * @return La liste des segments qui composent le chapeau.
+	 */
+	@Override
+	public ArrayList<Segment> dessiner() {
+		ArrayList<Segment> C = new ArrayList<Segment>();
+		C.add(new Segment(this.POINT1, this.POINT2));
+		C.add(new Segment(this.POINT2, this.POINT3));
+		return C;
+	}
+
+	/**
+	 * Renvoie le type de la forme chapeau qui est C
+	 *
+	 * @return (String) : chaine de caracteres donnant le type C
+	 */
+	@Override
+	public String typeForme() {
+		return "C";
+	}
+
+}
---
+++ src/ardoise/TestArdoise.java
@@ -0,0 +1,99 @@
+package ardoise;
+
+import ardoise.*;
+
+public class TestArdoise {
+	public static void main(String []args ){
+		
+		Ardoise ardoise = new Ardoise();
+	
+		PointPlan pt1 = new PointPlan(9,100);
+		PointPlan pt2 = new PointPlan(20,198);		
+		Forme tour = new Quadrilatere("tour", pt1, pt2);
+		
+		PointPlan pm1 = new PointPlan(3,14);
+		PointPlan pm2 = new PointPlan(43,3);
+		PointPlan pm3 = new PointPlan(112,14);
+		Forme montagne1 = new Triangle("montagne 1", pm1, pm2, pm3);
+		
+		PointPlan pm4 = new PointPlan(152,7);
+		PointPlan pm5 = new PointPlan(166,3);
+		PointPlan pm6 = new PointPlan(172,7);
+		Forme montagne2 = new Triangle("montagne 2", pm4, pm5, pm6);
+		
+		PointPlan po1 = new PointPlan(133,30);
+		PointPlan po2 = new PointPlan(136,32);
+		PointPlan po3 = new PointPlan(138,30);
+		Forme oiseau2 = new Chapeau("oiseau 2", po1, po2, po3);
+		
+		PointPlan po4 = new PointPlan(142,14);
+		PointPlan po5 = new PointPlan(144,17);
+		PointPlan po6 = new PointPlan(146,14);
+		Forme oiseau3 = new Chapeau("oiseau 3", po4, po5, po6);		
+		
+		PointPlan pb1 = new PointPlan(170,52);
+		PointPlan pb2= new PointPlan(173,45);
+		PointPlan pb3= new PointPlan(177,52);
+		Forme branche1 = new Chapeau("branche1",pb1,pb2,pb3);
+		
+		PointPlan pb4 = new PointPlan(177,52);
+		PointPlan pb5= new PointPlan(184,57);
+		PointPlan pb6= new PointPlan(177,60);
+		Forme branche2 = new Chapeau("branche1",pb4,pb5,pb6);
+		
+		PointPlan pb7 = new PointPlan(177,60);
+		PointPlan pb8= new PointPlan(174,66);
+		PointPlan pb9= new PointPlan(170,60);
+		Forme branche3 = new Chapeau("branche1",pb7,pb8,pb9);
+		
+		PointPlan pb10 = new PointPlan(170,60);
+		PointPlan pb11= new PointPlan(164,57);
+		PointPlan pb12= new PointPlan(170,52);
+		Forme branche4 = new Chapeau("branche1",pb10,pb11,pb12);
+		
+		ArrayList<Forme> desEtoiles = new ArrayList<Forme>();
+		desEtoiles.add(branche1);
+		desEtoiles.add(branche2);
+		desEtoiles.add(branche3);
+		desEtoiles.add(branche4);
+		
+		Forme Etoile = new FusionForme("etoiles",desEtoiles);
+		
+		
+		PointPlan pma1 = new PointPlan(80,140);
+		PointPlan pma2 = new PointPlan(180,198);		
+		Forme CorpMaison = new Quadrilatere("CorpMaison", pma1, pma2);
+		
+		PointPlan pma3 = new PointPlan(120,170);
+		PointPlan pma4 = new PointPlan(140,198);	
+		Forme PorteMaison = new Quadrilatere("PorteMaison", pma3, pma4);
+		
+		PointPlan pma5 = new PointPlan(80,140);
+		PointPlan pma6 = new PointPlan(130,100);	
+		PointPlan pma7 = new PointPlan(180,140);	
+		Forme ToitMaison = new Chapeau("PorteMaison", pma5, pma6,pma7);
+		
+		ArrayList<Forme> Maisons = new ArrayList<Forme>();
+		Maisons.add(PorteMaison);
+		Maisons.add(ToitMaison);
+		Maisons.add(CorpMaison);
+		
+		Forme Maison = new FusionForme("Maison",Maisons);
+		
+		
+		ardoise.dessinerGraphique();
+	
+		ardoise.ajouterForme(tour);
+		ardoise.ajouterForme(montagne1);
+		ardoise.ajouterForme(montagne2);
+		ardoise.ajouterForme(oiseau2);
+		ardoise.ajouterForme(oiseau3);
+		ardoise.ajouterForme(oiseau2);
+		ardoise.ajouterForme(Etoile);
+		ardoise.ajouterForme(Maison);
+		
+		
+		ardoise.test();
+
+ }
+ }
